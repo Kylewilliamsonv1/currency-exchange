c5de72794e4142b75b70a7bef899e3d6ed46dfcb
Kylewilliamsonv1 <84644678+Kylewilliamsonv1@users.noreply.github.com>
2021-07-11 17:38:59 -0700

    edits

--- src/main.js
+++ src/main.js
@@ -4,19 +4,20 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
 import CurrencyExchange from './currency.js';
 
-// function clearEntry () {
-//   $('#inputAmount').val("");
-//   $('#inputCurrency').val("");
-// }
+function clearEntry () {
+  $('#inputAmount').val("");
+  $('#inputCurrency').val("");
+}
 
 $("form#currencyCal").submit(function(event) {
   event.preventDefault();
   let currencyType = $('#inputCurrency').val();
   let amount = parseInt($('#inputAmount').val());
-  // clearEntry();
+  clearEntry();
   CurrencyExchange.getExchange(currencyType,amount).then(function(response) {
+    $("#output-currencyType").text(currencyType);
     $("#output-amount").text(response);
-    console.log(response);
+    $(".output").show();
   });
 });
 
